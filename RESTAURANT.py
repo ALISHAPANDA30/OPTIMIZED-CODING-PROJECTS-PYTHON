2dda2c7a08a2badf72f1b084512f49b3d216a6a1
Vineet Padole <vineetpadole137@gmail.com>
2026-02-02 15:43:53 +0000

    Added a menu and a loop

--- RESTAURANT.py
+++ RESTAURANT.py
@@ -1,5 +1,14 @@
+total = 0
+print("==MENU==")
+print("____RESTAURANT MENU____")
+print("1...APPLE............500")
+print("2...BANANA...........200")
+print("3...GRAPE............300")
+print("4...MANGO............400")
+print("q...Exit the Menu.......") 
 my_dict = {"1": ("apple", 500),
     "2": ("banana", 200),
     "3": ("grape", 300),
     "4": ("mango", 400)}
-print(my_dict)
+while True:
+    pass
