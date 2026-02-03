5a8c1867056de14b22aae15b77d01f958d74d6bd
ALISHA PANDA <alishapanda9003@gmail.com>
2026-02-03 15:06:53 +0000

    def

--- atm.py
+++ atm.py
@@ -5,6 +5,12 @@ class hdfcbank:
             self.exit_q = exit_q
             self.deposit_money = deposit_money
             self.withdraw_money = withdraw_money
-            self.checkbal_money = checkbal_money            
-    def 
+            self.checkbal_money = checkbal_money 
+               
+    def bank_info(self):
+        user_input=input("ENTER YOUR CHOICE OR q TO QUIT:")
+        if user_input == q:
+                return (f"THANKS FOR VISITING US!!{self.exit_q})
+                break
+        elif user_input == deposit
                 
