01d6efbe125ef28ff8e342b185a4cd5c785bc571
ALISHA PANDA <alishapanda9003@gmail.com>
2026-02-03 15:27:51 +0000

    steps

--- atm.py
+++ atm.py
@@ -1,16 +1,26 @@
 #atm menu driven program
 #Menu: Deposit, Withdraw, Check Balance, Exit. Keep running until q.
 class hdfcbank:
-    def _init_ (self,deposit_money,withdraw_money,checkbal_money,exit_q):
+    def __init__(self,deposit_money,withdraw_money,checkbal_money,exit_q):
             self.exit_q = exit_q
             self.deposit_money = deposit_money
             self.withdraw_money = withdraw_money
             self.checkbal_money = checkbal_money 
                
     def bank_info(self):
-        user_input=input("ENTER YOUR CHOICE OR q TO QUIT:")
-        if user_input == q:
-                return (f"THANKS FOR VISITING US!!{self.exit_q})
-                break
-        elif user_input == deposit
+        while True:
+                user_input=input("ENTER YOUR CHOICE OR q TO QUIT:")
+                if user_input == "q":
+                       return (f"THANKS FOR VISITING US!!{self.exit_q}")
+                elif user_input == "deposit":
+                       return (f"YOUR DEPOSIT AMOUNT IS:{self.deposit_money}")
+                elif user_input == "withdraw":
+                       return (f"YOUR WITHDRAW AMOUNT IS:{self.withdraw_money}")
+                elif user_input == "check balance":
+                       return (f"YOUR BALANCE AMOUNT IS:{self.checkbal_money}")
+                else:
+                        return ("INVALID INPUT")
+        
+hdfc = hdfcbank(1000,500,1500,"QUIT")
+print(hdfc.bank_info())
                 
