e87b67af2bd69a27c791a7595bd4d045622ac24f
ALISHA PANDA <alishapanda9003@gmail.com>
2026-02-03 14:54:51 +0000

    ATM PROGRAM

---
+++ atm.py
@@ -0,0 +1,10 @@
+#atm menu driven program
+#Menu: Deposit, Withdraw, Check Balance, Exit. Keep running until q.
+class hdfcbank:
+    def _init_ (self,deposit_money,withdraw_money,checkbal_money,exit_q):
+            self.exit_q = exit_q
+            self.deposit_money = deposit_money
+            self.withdraw_money = withdraw_money
+            self.checkbal_money = checkbal_money            
+    def 
+                
