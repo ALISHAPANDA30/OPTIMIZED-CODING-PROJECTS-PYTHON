3ceed150b5451c9f763137cd82fbb387a1b04508
ALISHA PANDA <alishapanda9003@gmail.com>
2026-02-05 17:43:19 +0000

    add dictionary

--- railway.py
+++ railway.py
@@ -1,3 +1,6 @@
 #Railway Ticket System
 #Menu: First class, Second class, Sleeper. User selects and total fare is calculated.
+menu = {"1st Class": 1500, "2nd Class": 1000, "Sleeper": 500}
+
+        
 
